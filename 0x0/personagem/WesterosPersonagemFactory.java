7dbe80717a2bb2ecbc33e1df424bc83e11ea6deb
luisotv <luisotavio@ciandt.com>
2022-04-25 18:05:58 -0300

    task 0

---
+++ 0x0/personagem/WesterosPersonagemFactory.java
@@ -0,0 +1,19 @@
+public class WesterosPersonagemFactory extends PersonagemFactory {
+    @Override
+    public Personagem createPersonagem(TipoPersonagem tipoPersonagem, String nome) {
+
+        if (tipoPersonagem == TipoPersonagem.MAGO) {
+
+            return new Mago(nome, 8, 3, 3, 3, 4);
+        } else if (tipoPersonagem == TipoPersonagem.LADRAO) {
+
+            return new Ladrao(nome, 1, 8, 7, 7, 10);
+        } else if (tipoPersonagem == TipoPersonagem.GUERREIRO) {
+
+            return new Guerreiro(nome, 0, 9, 7, 10, 7);
+        } else {
+
+            return null;
+        }
+    }
+}
