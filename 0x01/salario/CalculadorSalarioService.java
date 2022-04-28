3f3a76781b96c50df650135f04539c3ce7e3a196
luisotv <luisotavio@ciandt.com>
2022-04-28 10:15:27 -0300

    task 0

---
+++ 0x01/salario/CalculadorSalarioService.java
@@ -0,0 +1,40 @@
+import java.util.UUID;
+
+public class CalculadorSalarioService {
+
+    public UUID uuid = UUID.randomUUID();
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+
+    private CalculadorSalarioService() {
+    }
+
+    private static CalculadorSalarioService instancia;
+
+
+
+    public static CalculadorSalarioService getCalculadorSalarioService(){
+
+        if (instancia == null) {
+
+
+            instancia = new CalculadorSalarioService();
+            return instancia;
+
+
+        } else {
+
+            return instancia;
+
+        }
+
+    }
+
+    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+
+        return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
+    }
+}
