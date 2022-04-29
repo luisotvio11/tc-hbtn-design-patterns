840d352cb0cb8ca4e3cc96ea7ad3bce70e7f5e6f
luisotv <luisotavio@ciandt.com>
2022-04-29 10:00:44 -0300

    task 0

--- 0x02/lanchonete/PedidoAbstractBuilder.java
+++ 0x02/lanchonete/PedidoAbstractBuilder.java
@@ -1,8 +1,8 @@
 public abstract class PedidoAbstractBuilder {
 
     public abstract void setLanche(TipoLanche tipo);
-    public abstract void setBatata (TamanhoBatata tamanho);
-    public abstract void setBrinde (TipoBrinde tipo);
-    public abstract void setBebida (TipoBebida tipo);
+    public abstract void setBatata(TamanhoBatata tamanho);;
+    public abstract void setBrinde(TipoBrinde tipo);
+    public abstract void setBebida(TipoBebida tipo);
 
 }
