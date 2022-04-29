5ace20433f951c27b65971aa195153d6ca0ec58e
luisotv <luisotavio@ciandt.com>
2022-04-29 09:28:29 -0300

    task 0

--- 0x02/lanchonete/Pedido.java
+++ 0x02/lanchonete/Pedido.java
@@ -2,13 +2,10 @@ import java.util.HashSet;
 
 public class Pedido {
 
-    private HashSet <ItemPedido> itensDentroCaixa;
-    private HashSet <ItemPedido> itensForaCaixa;
+    private HashSet <ItemPedido> itensDentroCaixa = new HashSet<> ();
+    private HashSet <ItemPedido> itensForaCaixa = new HashSet<> ();
 
-    public Pedido() {
-        this.itensDentroCaixa = new HashSet<>();
-        this.itensForaCaixa = new HashSet<>();
-    }
+
 
     public void  adicionarItemDentroCaixa(ItemPedido item) {
 
